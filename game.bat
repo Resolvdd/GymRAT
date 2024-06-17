5639c65753ed2622781e41cb0f0cf2a24a5627dd
Resolvdd <95631279+Resolvdd@users.noreply.github.com>
2024-06-17 15:03:28 +0100

    Update game.bat

--- game.bat
+++ game.bat
@@ -1,21 +1,68 @@
 @echo off
-title "Name Guesser"
+:: Set the code page to UTF-8
+chcp 65001 > nul
 
-:WelcomeScreen
-    color 1e
+:: Print the text
+echo  ███▄    █  ▄▄▄       ███▄ ▄███▓▓█████      ▄████  █    ██ ▓█████   ██████   ██████ ▓█████  ██▀███  
+echo  ██ ▀█   █ ▒████▄    ▓██▒▀█▀ ██▒▓█   ▀     ██▒ ▀█▒ ██  ▓██▒▓█   ▀ ▒██    ▒ ▒██    ▒ ▓█   ▀ ▓██ ▒ ██▒
+echo ▓██  ▀█ ██▒▒██  ▀█▄  ▓██    ▓██░▒███      ▒██░▄▄▄░▓██  ▒██░▒███   ░ ▓██▄   ░ ▓██▄   ▒███   ▓██ ░▄█ ▒
+echo ▓██▒  ▐▌██▒░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄    ░▓█  ██▓▓▓█  ░██░▒▓█  ▄   ▒   ██▒  ▒   ██▒▒▓█  ▄ ▒██▀▀█▄  
+echo ▒██░   ▓██░ ▓█   ▓██▒▒██▒   ░██▒░▒████▒   ░▒▓███▀▒▒▒█████▓ ░▒████▒▒██████▒▒▒██████▒▒░▒████▒░██▓ ▒██▒
+echo ░ ▒░   ▒ ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░    ░▒   ▒ ░▒▓▒ ▒ ▒ ░░ ▒░ ░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░░ ▒░ ░░ ▒▓ ░▒▓░
+echo ░ ░░   ░ ▒░  ▒   ▒▒ ░░  ░      ░ ░ ░  ░     ░   ░ ░░▒░ ░ ░  ░ ░  ░░ ░▒  ░ ░░ ░▒  ░ ░ ░ ░  ░  ░▒ ░ ▒░
+echo    ░   ░ ░   ░   ▒   ░      ░      ░      ░ ░   ░  ░░░ ░ ░    ░   ░  ░  ░  ░  ░  ░     ░     ░░   ░ 
+echo          ░       ░  ░       ░      ░  ░         ░    ░        ░  ░      ░        ░     ░  ░   ░     
+                                                                                                    
+
+color 03
+
+:delay
+	
 
 
-    echo.
-    echo  :=------------------------------------------------------------------------=:
-    echo.
-    echo        ______  _             ______                    ______             
-    echo       /_  __/ ^(_^) _____     /_  __/ ____ _  _____     /_  __/ ____   ___ 
-    echo        / /   / / / ___/      / /   / __ `/ / ___/      / /   / __ \ / _ \
-    echo       / /   / / / /__       / /   / /_/ / / /__       / /   / /_/ //  __/
-    echo      /_/   /_/  \___/      /_/    \__,_/  \___/      /_/    \____/ \___/
-    echo.
-    echo.
-    echo.
-    echo                              Author : Psi505
-    echo.
-    echo  :=------------------------------------------------------------------------=:
+echo calculating the processor fundementals of the quantum defibrilator
+echo $0101011000$
+echo ██ 1.0 percent██
+timeout /t 2 > nul
+echo ██4.0 percent██
+timeout /t 1 > nul
+echo ██16.0 percent██
+timeout /t 3 > nul
+echo ██35.0 percent██
+echo ██45.0 percent██
+timeout /t 2 > nul
+echo ██69.0 percent██
+echo ██69.0 percent██
+timeout /t 1 > nul
+echo ██69 .0percent██
+echo █100 .0percent█
+echo █DONE█
+timeout /t 2 > nul
+echo INITIALIZING REFINEMENT
+echo DONE
+timeout /t 2 > nul
+echo CALCULATING NAME
+
+set /p name="Enter your name: "
+echo calculating 69=nice
+echo 696969696969
+timeout /t 1 > nul
+echo 2345634526345
+timeout /t 1 > nul
+echo 97896789679
+echo 4678467846846864
+echo .
+timeout /t 1 > nul
+echo 75735734545858
+echo .
+timeout /t 1 > nul
+echo .
+echo .
+echo .
+timeout /t 1 > nul
+echo .
+echo .
+echo DONE
+
+echo your name is %NAME%
+pause
